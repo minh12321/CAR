74dc9a554b86175ea5579c7b78dce9e6ae65db12
minh12321 <caominhh725@gmail.com>
2026-06-24 14:08:03 +0700

    test hiệu năng

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Analytics />
   </React.StrictMode>,
 );
